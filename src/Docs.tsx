31e13b81f0a01fb74aa1f8bd3bcd29f61b48b0ed
devg1120 <devg1120@gmail.com>
2026-09-26 10:29:59 +0900

    up

--- src/Docs.tsx
+++ src/Docs.tsx
@@ -1,32 +1,11 @@
+import Doc_01 from "./docs/doc_01"
+import Doc_02 from "./docs/doc_02"
+import Doc_03 from "./docs/doc_03"
+import Doc_04 from "./docs/doc_04"
+
 export const Docs = [
-  {
-    title: "case A",
-    code: `
-site "My Tech Blog" {
-    author "John Doe" email="john@example.com"
-}
-  
-`,
-  },
-  {
-    title: "case B",
-    code: `
-project "ポートフォリオサイト制作" status="進行中" {
-    backlog {
-        task "デザインの作成" priority="high" cost=5
-        task "ドメインの取得" priority="low" cost=1
-    }
-    todo {
-        task "SolidJSのセットアップ" priority="medium" cost=2
-        task "kdljsのインテグレーション" priority="high" cost=3
-    }
-    in-progress {
-        task "コンポーネントの再帰レンダラーの実装" priority="high" cost=4
-    }
-    done {
-        task "Viteプロジェクトの立ち上げ" cost=1
-    }
-}
-  `,
-  },
+  Doc_01,
+  Doc_02,
+  Doc_03,
+  Doc_04,
 ];
---
+++ src/docs/doc_03.tsx
@@ -0,0 +1,13 @@
+const Docs = 
+{
+title: "TABLE 1",
+code: `
+row rank=1 product="スマートフォン" price=98000 category="ガジェット" sales=1200
+row rank=2 product="ワイヤレスイヤホン" price=15000 category="オーディオ" sales=3500
+row rank=3 product="スマートウォッチ" price=32000 category="ガジェット" sales=850
+row rank=4 product="4Kモニター" price=45000 category="PC周辺機器" sales=400
+`
+}
+
+export default Docs;
+
