1dc8d0ea233748aa482524650906f4c4d8829973
devg1120 <devg1120@gmail.com>
2026-09-26 11:44:05 +0900

    up

--- src_old1/Docs.tsx
+++ src_old1/Docs.tsx
@@ -3,9 +3,6 @@ import Doc_02 from "./docs/doc_02"
 import Doc_03 from "./docs/doc_03"
 import Doc_04 from "./docs/doc_04"
 import Doc_05 from "./docs/doc_05"
-import Doc_06 from "./docs/doc_06"
-import Doc_07 from "./docs/doc_07"
-import Doc_08 from "./docs/doc_08"
 
 export const Docs = [
   Doc_01,
@@ -13,7 +10,4 @@ export const Docs = [
   Doc_03,
   Doc_04,
   Doc_05,
-  Doc_06,
-  Doc_07,
-  Doc_08,
 ];
